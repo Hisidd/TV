c22e72f0bf57362a12e3c894c4d9268599fde25c
Hisidd <Belalsiddiquei40@gmail.com>
2021-11-25 21:54:36 +0530

    update

---
+++ tv.js
@@ -0,0 +1,15 @@
+function myFunction2(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/uDDO4U2r3x0";
+}//ARY Qtv
+
+function myFunction1(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/_u_fOQRoM5o";
+}//Masti Tv
+
+function myFunction3(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/YEYaHrDzsMc";
+}//Recipe Tv
+
+function myFunction4(){
+    document.getElementById("myframe").src = "https://www.youtube.com/embed/MN8p-Vrn6G0";
+}//Recipe Tv
